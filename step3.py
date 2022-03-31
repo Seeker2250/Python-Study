287568315c988ed42e2383e4a25c331960090f1b
(Seeker) <a01044777016@gmail.com>
2022-03-31 18:21:34 +0900

    first commit

---
+++ step3.py
@@ -0,0 +1,48 @@
+a, b, c = 10, 20, 30
+a=b=c
+print(a)
+print(b)
+print(c)
+
+country = 'Korea'
+population = '50M'
+print("{} has about {} people" .format(country, population))
+i = 1.23456789
+j = 0.22222222
+k = 1.87654321
+l = 0.21314151
+print("{:.2}" .format(i))
+print("{:10.4}" .format(j))
+print("{}" .format(k))
+print("{:10.4}" .format(l))
+A, B, C, D = 2, 7, 3, 17
+print((A+B)*D//A)
+A, B = 3, 7
+C = B // A
+print (A, B, C)
+X, Y, Z, W = 11, 5, 7, 13
+print(W-(Y + Z))
+int_1 = 0
+int_2 = 3
+print("c:\\\'aa\\\"abc.txt")
+motto = "Enjoy my life"
+a = 13
+print (bin(a))
+print (oct(27))
+print (hex(94))
+print (hex(12648430))
+print (bin(14))
+print (bin(0b110))
+print (bin(0x110))
+print (True&False)
+name = "Julia"
+age = 21
+height = 162
+weight = 47
+major = "Mathematic"
+print("="*40)
+print("My name is {}".format(name))
+print("I'm {} years old".format(age))
+print("I'm {} cm and {} kg".format(height, weight))
+print("I major in {}".format(major))
+print("="*40)
---
+++ test.py
@@ -0,0 +1,35 @@
+print(1234567890)
+print("제 이름은 김준석입니다.")
+print('''그대는
+나에게 왔다가
+떠나가네...''')
+
+print("23Bc7")
+print("23" +  "Bc7")
+
+print('b', 'c', end = ' ')
+print('D')
+print('E', end = 'F')
+print("\n")
+
+x = 2
+y = 1
+z = x + y
+print (z)
+
+A=2
+B=3
+print(A*2 + B*2)
+
+print((20*5) + (34-2) - (72 / 5))
+x = 1
+y = 2
+z = 3
+print((1*6) + (y*2))
+print(10 * ((1*6) + (y*2)))
+print(100 * ((1*6) + (y*2)))
+
+A = 2.5
+B = 4
+C = ((4 * A) + B)
+print (C) 
